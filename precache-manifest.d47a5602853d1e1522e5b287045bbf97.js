7141862acfca94ecb87b657ad07f1b25d1ecf529
Shuvo <meghsohor@yahoo.com>
2020-07-23 21:23:01 +0600

    Updates

---
+++ precache-manifest.d47a5602853d1e1522e5b287045bbf97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4195885671a2376700f7a0fcd0858ae",
+    "url": "/react-github-job/index.html"
+  },
+  {
+    "revision": "5cb6bec015691cb95781",
+    "url": "/react-github-job/static/css/main.8c44fb98.chunk.css"
+  },
+  {
+    "revision": "5acb43eee86b1a4ff5cb",
+    "url": "/react-github-job/static/js/2.3e58bf43.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/react-github-job/static/js/2.3e58bf43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb6bec015691cb95781",
+    "url": "/react-github-job/static/js/main.17a4c1e8.chunk.js"
+  },
+  {
+    "revision": "88c794773a045f4896c0",
+    "url": "/react-github-job/static/js/runtime-main.f7fa2eb8.js"
+  }
+]);
